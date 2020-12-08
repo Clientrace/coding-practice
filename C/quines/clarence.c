95d9da58ad4e8ef986c23db42cf7d081a3dbc189
Kim Clarence Penaflor <kimc.penaflor@gmail.com>
2020-12-09 02:35:43 +0800

    wip: clarence quine in c

---
+++ C/quines/clarence.c
@@ -0,0 +1,39 @@
+#include<stdio.h>  // Quine in C, took me half a day just to implement. Displays my animated name and it's own source code. In short, a complex useless code :D!sry
+#include<unistd.h> // Kim Clarence Penaflor - [ kimc.penaflor@gmail.com ] Nothing to comment here, just wanna fill the blanks to hide my ugly obfuscated code.. :(
+#include<string.h> // Nothing here also. BLAH blah blah BLAH blah, uSeFUl comments. BLah, blah, blah, blah blah. If you're someone from google, hire me pls..,,, jk
+#include<stdlib.h> // Still trying to fill the void in my code and my life, haha (See what i did there). Took me hours to made this art, oh well. #nerd	   ¯\_(ツ)_/¯   
+static char map[]="\n\\\"scn=";void main(){const char*code1="#include<stdio.h>  // Quine in C, took me half a day just to implement. Displays my animated name and ";
+const char*code2="it's own source code. In short, a complex useless code :D!sry..%c#include<unistd.h> // Kim Clarence Penaflor - [ kimc.penaflor@gmail.com ] Nothin";
+const char*code3="g to comment here, just wanna fill the blanks to hide my ugly obfuscated code.. :(%c#include<string.h> // Nothing here also. BLAH blah blah BLAH ";
+const char*code4="blah, uSeFUl comments. BLah, blah, blah, blah blah. If you're someone from google, hire me pls..,,, jk%c#include<stdlib.h> // Still trying to fill";
+const char*code5=" the void in my code and my life, haha (See what i did there). Took me hours to made this art, oh well. #nerd    ¯\\_(ツ)_/¯   %cstatic char map[]";
+const char*code6="=%c%cn%c%c%c%cscn=%c;void main(){const char*code1=%c%s%c;%cconst char*code2=%c%s%c;%cconst char*code3=%c%s%c;%cconst char*code4=%c%s%c;%cconst cha";
+const char*code7="r*code5=%c%s%c;%cconst char*code6=%c%s%c;%cconst char*code7=%c%s%c;";const char*co7="const char*co7=%c%s%c;char c[]=%c%cu2588%c;";char c[]="\u2588";
+const char*code8="%cconst char*code8=%c%s%c;system(%cclear%c);const char*t=%c%s%c;";system("clear");const char*t="s1c6s1c2s1s6c5s1s1c6s1s1c7s1c3s1s3c2s1s1c6s1c7n1c2";
+const char*code9="%cconst char*code9=%c%s%c;const char*t2=%c%s%c;";const char*t2="s6c2s1s5c2s3c2s1c2s3c2s1c2s6c4s1s2c2s1c2s6c2s5n1c2s1s5c2s1s5c7s1c6s2c5s1s2c2s1c2s1";
+const char*cod10="%cconst char*cod10=%c%s%c;const char*t3=%c%s%c;";const char*t3="s1c2s1c2s1s5c5s2n1c2s1s5c2s1s5c2s3c2s1c2s3c2s1c2s4s2c2s2c2s1c2s1c2s1s5c2s5n1s1c6s1";
+const char*cod11="%cconst char*cod11=%c%s%c;const char*t4=%c%s%c;char n[262];%cconst cha";const char*t4="c7s1c2s1s2c2s1c2s1s2c2s1c7s1c2s1s1s1c4s2c6s1c7n1";char n[262];
+const char*cod12="r*cod12=%c%s%c;strcat(n,t);strcat(n,t2);strcat(n,t3);strcat(n,t4);";strcat(n,t);strcat(n,t2);strcat(n,t3);strcat(n,t4);for(int i=0;i<strlen(n)-2;i+=
+2){const int l=atoi(&n[i+1]);for(int j=0;j<l;j++){if(n[i]==map[3]){printf("%c",map[6]);}if(n[i]==map[4]){printf("%s",c);}if(n[i]==map[5]){printf("%c",map[0]);}usleep(
+50000);fflush(stdout);};}system("clear");const char*cod13="for(int i=0;i<strlen(n)-2;i+=%c2){const int l=atoi(&n[i+1]);for(int j=0;j<1;j++){if(n[i]==map[3]){printf(";
+const char*cod14="%c%%c%c,map[6]);}if(n[i]==map[4]){printf(%c%%s%c,c);}if(n[i]==map[5]){printf(%c%%c%c,map[0]);}usleep(%c50000);fflush(stdout);};}system(%cclear%c);co";
+const char*cod15="nst char*cod13=%c%s%c;%cconst char*cod14=%c%s%c;%cconst char*cod15=%c%s%c;printf(%c%%s%c,code1);printf(code2,map";printf("%s",code1);printf(code2,map
+[0]); const char*cod16="%c[0]);const char*cod16=%c%s%c;printf(code3,map[0]);printf(code4,map[0]);printf(code5,";printf(code3,map[0]);printf(code4,map[0]);printf(code5,
+map[0]);const char*cod17="%cmap[0]);const char*cod17=%c%s%c;printf(code6,map[2],map[1],map[1],map[1],map[1],map[2";printf(code6,map[2],map[1],map[1],map[1],map[1],map[2
+],map[2],map[2],code1,map[2],map[0],map[2],code2,map[2],map[0],map[2],code3,map[2],map[0],map[2],code4,map[2],map[0]);
+
+printf(code7,map[2],code5,map[2],map[0],map[2],code6,map[2],map[0],map[2],code7,map[2],map[2],map[1],map[2],map[2],map[2]);
+printf(co7,map[2],co7,map[2],map[2],map[1],map[2]);
+printf(code8,map[0],map[2],code8,map[2],map[2],map[2],map[2],t,map[2]);
+printf(code9,map[0],map[2],code9,map[2],map[2],t2,map[2]);
+printf(cod10,map[0],map[2],cod10,map[2],map[2],t3,map[2]);
+printf(cod11,map[0],map[2],cod11,map[2],map[2],t4,map[2],map[0]);
+printf(cod12,map[2],cod12,map[2]);
+printf(cod13,map[0]);
+printf(cod14,map[2],map[2],map[2],map[2],map[2],map[2],map[0],map[2],map[2]);
+printf(cod15,map[2],cod13,map[2],map[0],map[2],cod14,map[2],map[0],map[2],cod15,map[2],map[2],map[2]);
+printf(cod16,map[0],map[2],cod16,map[2]);
+printf(cod17,map[0],map[2],cod17,map[2]);
+}
+
+
---
+++ convert.py
@@ -0,0 +1,32 @@
+
+line1 = " ██████ ██       █████  ██████  ███████ ███    ██  ██████ ███████"
+line2 = "██      ██      ██   ██ ██   ██ ██      ████   ██ ██      ██     "
+line3 = "██      ██      ███████ ██████  █████   ██ ██  ██ ██      █████  "
+line4 = "██      ██      ██   ██ ██   ██ ██      ██  ██ ██ ██      ██     "
+line5 = " ██████ ███████ ██   ██ ██   ██ ███████ ██   ████  ██████ ███████"
+clist = [line1,line2,line3,line4,line5]
+
+out = ''
+stack = []
+index = 0
+for l in clist:
+    for c in l:
+        if len(stack)>0:
+            if c != stack[-1]:
+                char = 'c' if stack[-1] == '█' else 's'
+                out += char+str(len(stack))
+                # print(out)
+                stack = []
+        stack.append(c)
+
+    
+    char = 'c' if stack[-1] == '█' else 's'
+    out += char +str(len(stack)) + 'n1'
+    stack = []
+
+
+print(out)
+
+                
+
+
