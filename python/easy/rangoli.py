3fac9b914bb20ff775a1805d68388d3415e193fe
Kim Clarence Penaflor <kimc.penaflor@gmail.com>
2021-03-01 10:30:53 +0800

    add rangoli hackerrank problem

---
+++ python/easy/rangoli.py
@@ -0,0 +1,40 @@
+
+# Alphabet Rangoli
+# Size 5
+# --------e--------
+# ------e-d-e------
+# ----e-d-c-d-e----
+# --e-d-c-b-c-d-e--
+# e-d-c-b-a-b-c-d-e
+# --e-d-c-b-c-d-e--
+# ----e-d-c-d-e----
+# ------e-d-e------
+# --------e--------
+
+def print_rangoli(size):
+    center_val = 97
+    for i in range(0,size):
+        pattern = []
+        p_val = center_val + size
+        for j in range(0,size):
+            if j  >= size- i - 1:
+                p_val -= 1
+                pattern.append(chr(p_val))
+            else:
+                pattern.append('-')
+        rev = pattern[:-1]
+        rev.reverse()
+        print('-'.join(pattern+rev))
+    for i in range(size-1,-1,-1):
+        pattern = []
+        p_val = center_val + size
+        for j in range(0,size):
+            if j  >= size - i - 1:
+                p_val -= 1
+                pattern.append(chr(p_val))
+            else:
+                pattern.append('-')
+        rev = pattern[:-1]
+        rev.reverse()
+        print('-'.join(pattern+rev))
+
